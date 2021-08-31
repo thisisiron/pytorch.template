0168a396117d998f0c1740f9ce1f5f91917982d1
Eon Kim <kimiron518@gmail.com>
2021-08-31 14:26:59 +0900

    add tensorboard

---
+++ utils/tensorboard.py
@@ -0,0 +1,6 @@
+def write_board(writer, status, iteration, imagebox=None, mode='train'):
+    for key, val in status.items():
+        writer.add_scalar(f"{key}/{mode}", val.avg, iteration)
+    if imagebox is not None:
+        for tag, imgs in imagebox.items():
+            writer.add_images(f"{mode}/{tag}", (imgs + 1) * .5, iteration)  # from (-1 ~ 1) to (0 ~ 1)
