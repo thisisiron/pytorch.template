28c84698d508ff1f3c9d675008e19fa6b4ca70f0
Eon Kim <kimiron518@gmail.com>
2021-08-31 14:27:37 +0900

    update utils

--- utils/optims.py
+++ utils/optims.py
@@ -1,13 +1,13 @@
 from torch import optim
 
 
-def get_optimizer(param, opt_name: str, lr: float, weight_decay: float):
-    if opt_name == 'SGD':
-        optimizer = optim.SGD(param, lr=lr, momentum=0.9, weight_decay=weight_decay, nesterov=True)
-    elif opt_name == 'Adam':
-        optimizer = optim.Adam(param, lr=lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
-    elif opt_name == 'RMSprop':
-        optimizer = optim.RMSprop(param, lr=lr, weight_decay=weight_decay)
+def get_optimizer(param, opt):
+    if opt['optimizer'] == 'sgd':
+        optimizer = optim.SGD(param, lr=opt['lr'], momentum=0.9, weight_decay=opt['weight_decay'], nesterov=True)
+    elif opt['optimizer'] == 'adam':
+        optimizer = optim.Adam(param, lr=opt['lr'], betas=(0.9, 0.999), eps=1e-08, weight_decay=opt['weight_decay'], amsgrad=False)
+    elif opt['optimizer'] == 'rmsprop':
+        optimizer = optim.RMSprop(param, lr=opt['lr'], weight_decay=opt['weight_decay'])
     else:
         raise NotImplementedError('The optimizer should be in [SGD, AdamP, ...]')
     return optimizer
